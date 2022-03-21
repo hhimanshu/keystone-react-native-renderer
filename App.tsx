cc82776b5bead15a6d5fd810fee8c6197b560f30
hhimanshu <dev.atwork@yahoo.com>
2022-03-21 09:21:30 -0700

    feat: add data from keystone and render as string

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const json = require('./data.json')
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Open up App.tsx to start creating your app!</Text>
+      <Text>{JSON.stringify(json)}</Text>
+      <StatusBar style="dark" />
     </View>
   );
 }
