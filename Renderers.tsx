7eb277c4470ab1716e29000052ea741af560685c
hhimanshu <dev.atwork@yahoo.com>
2022-03-21 11:42:32 -0700

    feat: override renderer styles

--- Renderers.tsx
+++ Renderers.tsx
@@ -1,16 +1,31 @@
-import { DocumentRendererProps } from '@keystone-6/document-renderer';
+import React from "react";
+
+import {DocumentRendererProps} from '@keystone-6/document-renderer';
 import {Text} from "react-native";
 
 export const renderers: DocumentRendererProps['renderers'] = {
     // use your editor's autocomplete to see what other renderers you can override
     inline: {
-        bold: ({ children }) => {
-            return <strong>{children}</strong>;
+        bold: ({children}) => {
+            return <Text style={{fontWeight: "bold"}}>{children}</Text>;
         },
+        italic: ({children}) => {
+            return <Text style={{fontStyle: "italic"}}>{children}</Text>;
+        },
+        underline: ({children}) => {
+            return <Text style={{textDecorationLine: "underline"}}>{children}</Text>;
+        },
+
     },
     block: {
-        paragraph: ({ children, textAlign }) => {
-            return <Text>{children}</Text>;
+        paragraph: ({children, textAlign}) => {
+            return <Text style={{textAlign: "center"}}>{children}</Text>;
         },
+        heading: ({level, children, textAlign}) => {
+            if (level === 1) {
+                return <Text style={{fontSize: 32}}>{children}</Text>
+            }
+            return <Text>{children}</Text>
+        }
     },
 };
